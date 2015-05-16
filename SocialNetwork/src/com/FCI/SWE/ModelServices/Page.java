9943fc34d6b08211e26d767ff781a96d8b44bc04
sarahhtammam <sarahhtammam@gmail.com>
2015-05-16 23:08:01 +0200

    Fixed issue #47 -- added javaDoc to class: Page

--- SocialNetwork/src/com/FCI/SWE/ModelServices/Page.java
+++ SocialNetwork/src/com/FCI/SWE/ModelServices/Page.java
@@ -10,6 +10,18 @@ import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 
+
+/**
+ * <h1>Page Entity class</h1>
+ * <p>
+ * This class will act as a model for page, it will holds page data
+ * </p>
+ *
+ * @author Sarah Hany
+ * @version 1.0
+ * @since 2014-04
+ */
+
 public class Page 
 {
 	
@@ -19,7 +31,14 @@ public class Page
 	public String category;
 	public int numberOfLikes;
 
-	
+	/**
+	 * Constructor accepts page data
+	 * 
+	 * @param name
+	 *            page name
+	 * @param owner
+	 *            page owner
+	 */
 	public Page (String name, String owner )
 	{
 		this.name=name;
@@ -27,6 +46,17 @@ public class Page
 		this.category=null;
 		numberOfLikes=1;
 	}
+	
+	/**
+	 * Constructor accepts page data
+	 * 
+	 * @param name
+	 *            page name
+	 * @param owner
+	 *            page owner
+	 * @param category
+	 *            page category
+	 */
 	public Page (String name, String owner, String category )
 	{
 		this.name=name;
@@ -35,7 +65,18 @@ public class Page
 		numberOfLikes=1;
 	}
 	
-	
+	/**
+	 * Constructor accepts page data
+	 * 
+	 * @param name
+	 *            page name
+	 * @param owner
+	 *            page owner
+	 * @param category
+	 *            page category
+	  * @param likes
+	 *            page number of likes
+	*/
 	public Page (String name, String owner, String category, int likes )
 	{
 		
@@ -45,6 +86,21 @@ public class Page
 		numberOfLikes=likes;
 	}
 	
+	/**
+	 * Constructor accepts page data
+	 * 
+	 * @param name
+	 *            page name
+	 * @param owner
+	 *            page owner
+	 * @param category
+	 *            page category
+	  * @param likes
+	 *            page number of likes
+	  * @param ID
+	 *            page ID
+	
+	*/
 	public Page (String name, String owner, String category, int likes, String ID )
 	{
 		
@@ -65,7 +121,7 @@ public class Page
 		return this.ID;
 	}
 	
-
+	
 	public static void incrementLikes (String pageID)
 	{
 		Page p= getPage(pageID);
@@ -74,6 +130,12 @@ public class Page
 		p.numberOfLikes=likes;
 		p.savePage();
 	}
+
+	/**
+	 * 
+	 * This method will save a new page using
+	 * page data if the page name doesn't exist from before
+	  */
 
 	public void savePage()
 	{
@@ -105,6 +167,14 @@ public class Page
 		lp.saveLiker();
 	}
 	
+	/**
+	 * 
+	 * This static method will get a specific page using page's ID
+	 * 
+	 * @param pageID
+	 *            String of the ID of the selected page
+	 * @return Constructed page entity
+	 */
 	public static Page getPage (String pageID)
 	{
 		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
@@ -130,7 +200,15 @@ public class Page
 		return null;
 	}	
 		
-	
+	/**
+	 * 
+	 * This static method will tell if a specific page exists
+	 * or not using it's name
+	 * 
+	 * @param pageName
+	 *            String of the name of the selected page
+	 * @return true if the page exists, otherwise false
+	 */
 	public static boolean checkPageEsistance (String pageName)
 	{
 		
@@ -150,7 +228,14 @@ public class Page
 		return false;
 				
 	}
-	
+	/**
+	 * 
+	 * This static method will get a specific page using page's name
+	 * 
+	 * @param pageName
+	 *            String of the Name of the selected page
+	 * @return Constructed page entity
+	 */
 	public static Page getPage_byName (String pageName)
 	{
 		
@@ -178,7 +263,14 @@ public class Page
 			
 			
 	}
-	
+	/**
+	 * 
+	 * This static method will get the owner of a specific page using page's ID
+	 * 
+	 * @param pageID
+	 *            String of the ID of the selected page
+	 * @return String of page's owner name
+	 */
 	public static String getPageOwner (String pageID)
 	{
 		
@@ -202,7 +294,13 @@ public class Page
 
 
 
-	
+	/**
+	 * 
+	 * This static method will get all pages in the datastore
+	 * 
+	 * @return Vector of page entity
+	 * containing all pages in datasotre
+	 */
 	public static Vector<Page> getAllPages()
 	{
 		Vector<Page> pages = new Vector<Page>();
@@ -225,6 +323,18 @@ public class Page
 		return pages;
 	}
 	
+
+	/**
+	 * 
+	 * This static method will tell if a specific 
+	 * user is the owner of the page or not 
+	 *  
+	 * @param pageName
+	 *            String of the name of the selected page
+	 * @param owner
+	 *            String of the name of the user
+	 * @return true if the user is the owner of the page, otherwise false
+	 */
 	public static boolean isOwner(String pageName, String owner) {
 		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 		
