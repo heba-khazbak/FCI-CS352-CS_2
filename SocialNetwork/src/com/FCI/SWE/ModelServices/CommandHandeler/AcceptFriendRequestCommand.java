2b083be38f9f89c9bb5f3db3ecd75c9bd2c24d1a
dalia-maher <dalia_2412@yahoo.com>
2015-05-18 15:55:01 +0200

    fixed issue #49 - javaDoc added

--- SocialNetwork/src/com/FCI/SWE/ModelServices/CommandHandeler/AcceptFriendRequestCommand.java
+++ SocialNetwork/src/com/FCI/SWE/ModelServices/CommandHandeler/AcceptFriendRequestCommand.java
@@ -2,12 +2,19 @@ package com.FCI.SWE.ModelServices.CommandHandeler;
 
 public class AcceptFriendRequestCommand implements ICommand {
 	FriendRequestReceiver Receiver;
+
+	/**
+	 * This function calls the acceptFriendService
+	 * @param ID
+	 *            Notification ID
+	 * @return String status
+	 */
 	@Override
 	public String execute(String ID) {
 		// TODO Auto-generated method stub
 		Receiver = new FriendRequestReceiver(ID);
 		return Receiver.acceptFriendService();
-		
+
 	}
 
 }
--- SocialNetwork/src/com/FCI/SWE/ModelServices/CommandHandeler/ICommand.java
+++ SocialNetwork/src/com/FCI/SWE/ModelServices/CommandHandeler/ICommand.java
@@ -2,6 +2,12 @@ package com.FCI.SWE.ModelServices.CommandHandeler;
 
 public interface ICommand {
 	
+	/**
+	 * This function should be implemented by derived classes
+	 * @param ID
+	 *            Notification ID
+	 * @return String status
+	 */
 	public String execute(String ID);
 
 }
